8b4dc6812d0922de1bf8b5fbe573003c605c7719
Alex Nguyễn <alex@exgo.vn>
2019-01-27 22:01:44 +0700

    Implement adding new task

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo"/>
     <Todos v-bind:data="todo_list" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
+import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 
 export default {
   name: 'app',
   components: {
-    Todos
+    Header,
+    Todos,
+    AddTodo
   },
   data() {
     return {
@@ -36,6 +42,9 @@ export default {
   methods:{
     deleteTodo(id){
       this.todo_list = this.todo_list.filter(todo => todo.id !== id)
+    },
+    addTodo(newTodo){
+      this.todo_list = [...this.todo_list,newTodo]
     }
   }
 }
@@ -50,5 +59,16 @@ export default {
   body {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
   }
 </style>
